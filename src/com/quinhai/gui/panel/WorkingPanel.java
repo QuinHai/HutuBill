da296577b8b107b418aab275028869e9c6a28b06
嘉海 宋 <song199808@outlook.com>
2018-07-16 18:22:56 +0800

    Revert "A bill manage system"
    This reverts commit 4d1cff6aa07300070efccecf79c3aac72f17457e.

--- src/com/quinhai/gui/panel/WorkingPanel.java
+++
@@ -1,8 +0,0 @@
-package com.quinhai.gui.panel;
-
-import javax.swing.JPanel;
-
-public abstract class WorkingPanel extends JPanel{
-	public abstract void addListener();
-	public abstract void updateData();
-}
